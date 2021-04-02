443f53e8de35836789cc5e41e3aee652b931dcc9
Ian Chong <iancwm@msn.com>
2021-04-02 20:49:11 +0800

    Q2 complete

--- SelfDrivingCar/SearchAgent.py
+++ SelfDrivingCar/SearchAgent.py
@@ -29,12 +29,177 @@ class SearchAgent(Agent):
             self.action_sequence=[]
         return action
 
-    def drive(self,goalstates,inputs):
+    def drive(self, goalstates, inputs):
         """Write your algorithm for self driving car"""
-        act_sequence=[]
-        return act_sequence        
+        # extract start and goal states from environment
+        path_list = []
+        destination_reached = []
+        action_order = []
+        path_lengths = []
+        start = self.state
 
+        # for all goal states do
+        #   goalReached,path= A Star(start, goal)
+        for goal in goalstates:
+            goalReached, path = self.AStar(start["location"], goal, inputs)
+            destination_reached.append(goalReached)
+            path_list.append(path)
+            path_lengths.append(len(path))
 
+        # Find best path from all paths received [1 path received for 1 goal]
+        if True in destination_reached:
+            # Best path, would the shortest path in case of goal is reachable 
+            best_path = [path_list[i] for i in range(len(path_list)) if (destination_reached[i] == True) and (len(path_list[i]) == min(path_lengths))]
+            action_order.extend(best_path)
+        else:
+            # otherwise it would be the longest path, how far traveled before being blocked 
+            longest_path = [path_list[i] for i in range(len(path_list)) if (len(path_list[i]) == max(path_lengths))]
+            action_order.extend(longest_path)
+
+        # Compute action sequence for best path
+        movements = {
+            (0, 3): "forward-3x", 
+            (0, 2): "forward-2x", 
+            (0, 1): "forward", 
+            (-1, 1): "left", 
+            (1, 1): "right", 
+            (0, 0): None}
+        try:
+            action_sequence = [movements[(action_order[0][i+1][0] - action_order[0][i][0], action_order[0][i+1][1] - action_order[0][i][1])] for i in range(len(action_order[0])-1)]
+        except:
+            action_sequence = [None]
+        # return action sequence
+        return action_sequence
+
+    def AStar(self, start, goal, state):
+        # closedSet = []
+        closedSet = []
+        # openSet = []
+        openSet = [start]
+        # cameFrom = []
+        cameFrom = {}
+        # gScore[start] = 0
+        gScore = {start: 0}
+        # fScore[start] = heuristic_cost_estimate(start, goal)
+        fScore = {start: self.heuristic_cost_estimate(start, goal)}
+        # current = []
+        current = []
+
+        # while openSet is not empty do
+        while len(openSet) > 0:
+            # current = the node in openSet having the lowest fScore value
+            min_idx = list(fScore.values()).index(min(fScore.values()))
+            current = list(fScore.keys())[min_idx]
+            # if current = goal then
+            if current == goal["location"]:
+                # return Goal_Reached, reconstruct_path(cameFrom, current)
+                return True, self.reconstruct_path(cameFrom, current)
+            # remove current from openSet and fScore
+            del fScore[current]
+            openSet.remove(current)
+            # add current to closedSet
+            closedSet.append(current)
+
+            # for all actions do
+            for action in ["forward-3x", "forward-2x", "forward", "left", "right"]:
+                # neighbor = applyAction(current, action)
+                neighbor = self.applyAction(current, action, state, goal["location"])
+                # if neighbor = current then
+                if neighbor == current:
+                    # path blocked, stop, evaluate next action
+                    continue
+                # tentative gScore = gScore[current] + Distance from start to neighbor
+                tentative_gScore = gScore[current] + 1
+                # if neighbor not in openSet then
+                if neighbor not in openSet:
+                    # Add neighbor in openSet
+                    openSet.append(neighbor)
+                # else if tentative gScore ≥ gScore[neighbor] then
+                elif tentative_gScore >= gScore[neighbor]:
+                    # stop, evaluate next action
+                    continue
+                # cameFrom[neighbor] = current
+                cameFrom[neighbor] = current
+                # gScore[neighbor] = tentative gScore
+                gScore[neighbor] = tentative_gScore
+                # fScore[neighbor] = gScore[neighbor] + heuristic_cost_estimate(neighbor, goal)
+                fScore[neighbor] = gScore[neighbor] + self.heuristic_cost_estimate(neighbor, goal)
+        
+        # if current = goal then
+        if current == goal["location"]:
+            # return Goal Reached, reconstruct path(cameFrom, current)
+            return True, self.reconstruct_path(cameFrom, current)
+        # else
+        else:
+            # return Goal Not Reachable, reconstruct_path(cameFrom, current)
+            return False, self.reconstruct_path(cameFrom, current)
+
+    def heuristic_cost_estimate(self, start, goal):
+        # return [how far ahead is goal state]/2
+        result = abs(goal["location"][1] - start[1]) + abs(goal["location"][0] - start[0]) / 2
+        return result
+
+    def applyAction(self, current, action, state, goal):
+        # define movement paths
+        movements = {
+            "forward-3x": [(0, 1), (0, 2), (0, 3)], 
+            "forward-2x": [(0, 1), (0, 2)], 
+            "forward": [(0, 1)], 
+            "left": [(-1, 0), (-1, 1)], 
+            "right": [(1, 0), (1, 1)], 
+            None: [(0, 0)]}
+        
+        # defining row and col
+        target_row = current[0]
+        target_col = current[1]
+        # define check if movement is valid
+        valid_move = True
+
+        # for each movement in the path
+        for movement in movements[action]:
+            # if movement is outside of the grid
+            if (target_row + movement[0] >= len(state)) or \
+                (target_row + movement[0] < 0) or \
+                (target_col + movement[1]) >= len(state[0]):
+                # return current location
+                return current
+            
+            # if movement has a car in the way
+            if state[target_row + movement[0]][target_col + movement[1]] == 1:
+                # declare check as False
+                valid_move = False
+                # stop all subsequent movements
+                break
+        
+        # if movement leads to a location where there will not be a path to the goal
+        if abs(target_row + movements[action][-1][0] - goal[0]) > abs(goal[1] - (target_col + movements[action][-1][1])):
+            # return current location
+            return current
+
+        # if movement is valid
+        if valid_move:
+            # add movement to the current row and col
+            target_row += movements[action][-1][0]
+            target_col += movements[action][-1][1]
+        # return endstate
+        return (target_row, target_col)
+    
+
+    def reconstruct_path(self, cameFrom, current):
+        # total_path = []
+        total_path = []
+        # while current in cameFrom.keys do
+        while current in cameFrom.keys():
+            # total_path.append(current)
+            total_path.append(current)
+            # current = cameFrom[current] total path.append(current) return total_path
+            current = cameFrom[current] 
+        # add the last node into list
+        total_path.append(current)
+        # flip list
+        total_path = total_path[::-1]
+        # return total_path
+        return total_path
 
     def update(self):
         """ The update function is called when a time step is completed in the 
